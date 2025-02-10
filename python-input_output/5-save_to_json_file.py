cc4f7109c882909a67165d4d45871fa9fe6b50c8
mrfouu <bryanweinegaessel@gmail.com>
2025-02-10 11:14:09 +0100

    test add def ins file 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """writes an object to a text file using json representation."""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
